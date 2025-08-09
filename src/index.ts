78acd462c1466cd0f94c055b0a65e0773e082269
Gabriel Rey <rgma1219@outlook.com>
2025-08-09 10:25:26 -0300

    Realicé los métodos que faltaban según consigna y pasa todos los test localmente

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import productos from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,36 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(nombreLista: string) {
+    super(nombreLista);
+    productos.forEach((prod) => {
+      this.addProduct(prod);
+    });
+  }
+
+  addProduct(product: Product) {
+    this.add(product);
+  }
+
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((c) => c.id === id);
+  }
+
+  removeProduct(id: number): void {
+    this.cosas = this.cosas.filter((c) => c.id !== id);
+  }
+
+  getSortedByPrice(order: string): Product[] {
+    // Implementación del método getSortedByPrice
+    const cosasAordenar = [...this.cosas];
+    if (order === "asc") {
+      return cosasAordenar.sort((c1, c2) => c1.price - c2.price);
+    } else {
+      return cosasAordenar.sort((c1, c2) => c2.price - c1.price);
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
